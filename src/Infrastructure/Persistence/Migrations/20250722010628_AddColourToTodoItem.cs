b85637e053a55432291f00fd2ba3bcf1a624bd56
weigle22 <emmanuelotacan95@gmail.com>
2025-07-22 10:13:04 +0800

    feat: added migration for the color feature

---
+++ src/Infrastructure/Persistence/Migrations/20250722010628_AddColourToTodoItem.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Todo_App.Infrastructure.Persistence.Migrations
+{
+    public partial class AddColourToTodoItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
