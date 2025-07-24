a9c2c09ec7cc73ebed5c7cf18d64b444d97d2067
weigle22 <emmanuelotacan95@gmail.com>
2025-07-24 11:20:46 +0800

    test: test related changes for second feature

--- tests/Application.IntegrationTests/TodoItems/Commands/UpdateTodoItemDetailTests.cs
+++ tests/Application.IntegrationTests/TodoItems/Commands/UpdateTodoItemDetailTests.cs
@@ -2,7 +2,6 @@
 using NUnit.Framework;
 using Todo_App.Application.Common.Exceptions;
 using Todo_App.Application.TodoItems.Commands.CreateTodoItem;
-using Todo_App.Application.TodoItems.Commands.UpdateTodoItem;
 using Todo_App.Application.TodoItems.Commands.UpdateTodoItemDetail;
 using Todo_App.Application.TodoLists.Commands.CreateTodoList;
 using Todo_App.Domain.Entities;
@@ -15,47 +14,58 @@ using static Testing;
 public class UpdateTodoItemDetailTests : BaseTestFixture
 {
     [Test]
-    public async Task ShouldRequireValidTodoItemId()
-    {
-        var command = new UpdateTodoItemCommand { Id = 99, Title = "New Title" };
-        await FluentActions.Invoking(() => SendAsync(command)).Should().ThrowAsync<NotFoundException>();
-    }
-
-    [Test]
-    public async Task ShouldUpdateTodoItem()
+    public async Task ShouldUpdateTodoItemDetails()
     {
         var userId = await RunAsDefaultUserAsync();
 
         var listId = await SendAsync(new CreateTodoListCommand
         {
-            Title = "New List"
+            Title = "Test List"
         });
 
         var itemId = await SendAsync(new CreateTodoItemCommand
         {
             ListId = listId,
-            Title = "New Item"
+            Title = "Initial Task",
+            Colour = "#ffffff",
+            Tags = new List<string> { "initial" }
         });
 
-        var command = new UpdateTodoItemDetailCommand
+        var updateCommand = new UpdateTodoItemDetailCommand
         {
             Id = itemId,
             ListId = listId,
-            Note = "A1",
-            Priority = PriorityLevel.High
+            Priority = PriorityLevel.High,
+            Note = "Updated note content",
+            Colour = "#ff0000",
+            Tags = new List<string> { "urgent", "work" }
         };
 
-        await SendAsync(command);
+        await SendAsync(updateCommand);
 
         var item = await FindAsync<TodoItem>(itemId);
 
         item.Should().NotBeNull();
-        item!.ListId.Should().Be(command.ListId);
-        item.Note.Should().Be(command.Note);
-        item.Priority.Should().Be(command.Priority);
-        item.LastModifiedBy.Should().NotBeNull();
-        item.LastModifiedBy.Should().Be(userId);
-        item.LastModified.Should().NotBeNull();
-        item.LastModified.Should().BeCloseTo(DateTime.Now, TimeSpan.FromMilliseconds(10000));
+        item!.Priority.Should().Be(PriorityLevel.High);
+        item.Note.Should().Be("Updated note content");
+        item.Colour.Should().Be("#ff0000");
+        item.Tags.Should().BeEquivalentTo(new List<string> { "urgent", "work" });
+    }
+
+    [Test]
+    public async Task ShouldThrowNotFoundExceptionWhenItemDoesNotExist()
+    {
+        var command = new UpdateTodoItemDetailCommand
+        {
+            Id = 9999, // Non-existent ID
+            ListId = 1,
+            Priority = PriorityLevel.Medium,
+            Note = "Doesn't matter",
+            Colour = "#123456",
+            Tags = new List<string> { "none" }
+        };
+
+        await FluentActions.Invoking(() =>
+            SendAsync(command)).Should().ThrowAsync<NotFoundException>();
     }
 }
